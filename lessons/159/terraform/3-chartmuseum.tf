e85e13e7d8950657d8ddd6208efe92dcdebb28d0
pratikargade <pratik.argade@coditas.com>
2023-08-05 16:36:10 +0530

    few update

--- lessons/159/terraform/3-chartmuseum.tf
+++ lessons/159/terraform/3-chartmuseum.tf
@@ -1,13 +1,13 @@
-# helm repo add chartmuseum https://chartmuseum.github.io/charts
-# helm install chartmuseum -n chartmuseum --create-namespace chartmuseum/chartmuseum --version 3.9.3 -f terraform/values/chartmuseum.yaml
-resource "helm_release" "chartmuseum" {
-  name = "chartmuseum"
+# # helm repo add chartmuseum https://chartmuseum.github.io/charts
+# # helm install chartmuseum -n chartmuseum --create-namespace chartmuseum/chartmuseum --version 3.9.3 -f terraform/values/chartmuseum.yaml
+# resource "helm_release" "chartmuseum" {
+#   name = "chartmuseum"
 
-  repository       = "https://chartmuseum.github.io/charts"
-  chart            = "chartmuseum"
-  namespace        = "chartmuseum"
-  create_namespace = true
-  version          = "3.9.3"
+#   repository       = "https://chartmuseum.github.io/charts"
+#   chart            = "chartmuseum"
+#   namespace        = "chartmuseum"
+#   create_namespace = true
+#   version          = "3.9.3"
 
-  values = [file("values/chartmuseum.yaml")]
-}
+#   values = [file("values/chartmuseum.yaml")]
+# }
--- lessons/159/terraform/4-sealed-secrets.tf
+++ lessons/159/terraform/4-sealed-secrets.tf
@@ -1,9 +1,9 @@
-resource "helm_release" "sealed_secrets" {
-  name = "sealed-secrets"
+# resource "helm_release" "sealed_secrets" {
+#   name = "sealed-secrets"
 
-  repository       = "https://charts.bitnami.com/bitnami"
-  chart            = "sealed-secrets"
-  namespace        = "kube-system"
-  create_namespace = true
-  version          = "1.2.11"
-}
+#   repository       = "https://charts.bitnami.com/bitnami"
+#   chart            = "sealed-secrets"
+#   namespace        = "kube-system"
+#   create_namespace = true
+#   version          = "1.2.11"
+# }
